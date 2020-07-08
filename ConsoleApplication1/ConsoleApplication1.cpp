b74fc2a779adcc0666626aa5a9c42ab5df587bbf
笑男 常 <1481167030@qq.com>
2020-07-08 18:06:12 +0800

    添加项目文件。

---
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -0,0 +1,189 @@
+﻿#include<Windows.h>
+#include<mmsystem.h>
+#include"DEVCAPS.h"
+#include"SYSTEM.h"
+#pragma comment(lib,"winmm.lib")
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance,
+	PSTR szCmdLine, int iCmdShow)
+{
+	static TCHAR szAppName[] = TEXT("SysMets3");
+	HWND hwnd;
+	MSG msg;
+	WNDCLASS wndclass;
+
+	wndclass.style = CS_HREDRAW | CS_VREDRAW;
+	wndclass.lpfnWndProc = WndProc;
+	wndclass.cbClsExtra = 0;
+	wndclass.cbWndExtra = 0;
+	wndclass.hInstance = hInstance;
+	wndclass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
+	wndclass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndclass.hbrBackground = (HBRUSH)GetStockObject(WHITE_BRUSH);
+	wndclass.lpszMenuName = NULL;
+	wndclass.lpszClassName = szAppName;
+
+	if (!RegisterClass(&wndclass))
+	{
+		MessageBox(NULL, TEXT("Program requires Windows NT!"),
+			szAppName, MB_ICONERROR);
+		return 0;
+	}
+	hwnd = CreateWindow(szAppName, TEXT("Get System Metrics No. 3"),
+		WS_OVERLAPPEDWINDOW | WS_VSCROLL | WS_HSCROLL,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		CW_USEDEFAULT, CW_USEDEFAULT,
+		NULL, NULL, hInstance, NULL);
+	ShowWindow(hwnd, iCmdShow);
+	UpdateWindow(hwnd);
+
+	while (GetMessage(&msg, NULL, 0, 0))
+	{
+		TranslateMessage(&msg);
+		DispatchMessage(&msg);
+	}
+	return msg.wParam;
+}
+LRESULT CALLBACK WndProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+	static int cxChar, cxCaps, cyChar, cxClient, cyClient, iMaxWidth;
+	HDC hdc;
+	INT i, x, y, iPaintBeg, iPaintEnd, iVertPos, iHorzPos;
+	PAINTSTRUCT ps;
+	SCROLLINFO si;
+	TCHAR szBuffer[10];
+	TEXTMETRIC tm;
+
+	switch (message)
+	{
+	case WM_CREATE:
+		hdc = GetDC(hwnd);
+		GetTextMetrics(hdc, &tm);
+		cxChar = tm.tmAveCharWidth;
+		cxCaps = (tm.tmPitchAndFamily & 1 ? 3 : 2) * cxChar / 2;
+		cyChar = tm.tmHeight + tm.tmExternalLeading;
+		ReleaseDC(hwnd, hdc);
+		//最大总长度
+		iMaxWidth = 22 * cxCaps + 40 * cxChar;
+		return 0;
+	case WM_SIZE:
+		cxClient = LOWORD(lParam);
+		cyClient = HIWORD(lParam);
+		si.cbSize = sizeof(SCROLLINFO);
+		si.fMask = SIF_RANGE | SIF_PAGE;
+		si.nMin = 0;
+		si.nMax = NUMLINES - 1;
+		si.nPage = cyClient / cyChar;
+		SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+
+		si.cbSize = sizeof SCROLLINFO;
+		si.fMask = SIF_RANGE | SIF_PAGE;
+		si.nMin = 0;
+		si.nMax = iMaxWidth / cxChar + 2;
+		si.nPage = cxClient / cxChar;
+		SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+		return 0;
+	case WM_PAINT:
+		hdc = BeginPaint(hwnd, &ps);
+		//TextOut(hdc, 800, 0, szBuffer, wsprintf(szBuffer, TEXT("%5d"), GetDeviceCaps(hdc, HORZRES)));
+		si.cbSize = sizeof SCROLLINFO;
+		si.fMask = SIF_POS;
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		iVertPos = si.nPos;
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		iHorzPos = si.nPos;
+		iPaintBeg = max(0, iVertPos + ps.rcPaint.top / cyChar);
+		iPaintEnd = min(iVertPos + ps.rcPaint.bottom / cyChar, NUMLINES - 1);
+
+		for (i = iPaintBeg; i <= iPaintEnd; i++) {
+			x = cxChar * (1 - iHorzPos);
+			y = cyChar * (i - iVertPos);
+			TextOut(hdc, x, y, sysmetrics[i].szLabel, lstrlen(sysmetrics[i].szLabel));
+			TextOut(hdc, x + 22 * cxCaps, y, sysmetrics[i].szDesc, lstrlen(sysmetrics[i].szDesc));
+			SetTextAlign(hdc, TA_RIGHT | TA_TOP);
+			TextOut(hdc, 22 * cxCaps + 40 * cxChar + x, y, szBuffer, wsprintf(szBuffer, TEXT("%5d"), GetSystemMetrics(sysmetrics[i].Index)));
+			SetTextAlign(hdc, TA_LEFT | TA_TOP);
+		}
+		EndPaint(hwnd, &ps);
+		return 0;
+	case WM_VSCROLL:
+		si.cbSize = sizeof SCROLLINFO;
+		si.fMask = SIF_ALL;
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		iVertPos = si.nPos;
+		switch (LOWORD(wParam)) {
+		case SB_TOP:
+			si.nPos = si.nMin;
+			break;
+		case SB_BOTTOM:
+			si.nPos = si.nMax;
+			break;
+		case SB_LINEUP:
+			si.nPos--;
+			break;
+		case SB_LINEDOWN:
+			si.nPos++;
+			break;
+		case SB_PAGEUP:
+			si.nPos -= si.nPage;
+			break;
+		case SB_PAGEDOWN:
+			si.nPos += si.nPage;
+		case SB_THUMBTRACK:
+			si.nPos = si.nTrackPos;
+			break;
+		default:
+			break;
+		}
+		si.fMask = SIF_POS;
+		SetScrollInfo(hwnd, SB_VERT, &si, TRUE);
+		GetScrollInfo(hwnd, SB_VERT, &si);
+		if (si.nPos != iVertPos) {
+			ScrollWindow(hwnd, 0, cyChar * (iVertPos - si.nPos), NULL, NULL);
+			UpdateWindow(hwnd);
+		}
+		return 0;
+	case WM_HSCROLL:
+		si.cbSize = sizeof SCROLLINFO;
+		si.fMask = SIF_ALL;
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		iHorzPos = si.nPos;
+		switch (LOWORD(wParam)) {
+		case SB_LEFT:
+			si.nPos = si.nMin;
+			break;
+		case SB_RIGHT:
+			si.nPos = si.nMax;
+			break;
+		case SB_LINELEFT:
+			si.nPos--;
+			break;
+		case SB_LINERIGHT:
+			si.nPos++;
+			break;
+		case SB_PAGELEFT:
+			si.nPos -= si.nPage;
+			break;
+		case SB_PAGERIGHT:
+			si.nPos += si.nPage;
+			break;
+		case SB_THUMBTRACK:
+			si.nPos = si.nTrackPos;
+			break;
+		default:
+			break;
+		}
+		si.fMask = SIF_POS;
+		SetScrollInfo(hwnd, SB_HORZ, &si, TRUE);
+		GetScrollInfo(hwnd, SB_HORZ, &si);
+		if (si.nPos != iHorzPos) {
+			ScrollWindow(hwnd, cxChar * (iHorzPos - si.nPos), 0, NULL, NULL);
+		}
+		return 0;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		return 0;
+	}
+	return DefWindowProc(hwnd, message, wParam, lParam);
+}
